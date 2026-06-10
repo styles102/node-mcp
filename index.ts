4a0f2596cfe8977830f6ec03abb5df2c3f37f49b
Liam Styles <liam.styles@distinction.co.uk>
2026-06-10 16:00:12 +0100

    HTTP transport setup

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+import { randomUUID } from 'crypto';
 import 'dotenv/config';
+import express from 'express';
 import { addTool } from "./tools/opperations/add.js";
 import { divideTool } from "./tools/opperations/divide.js";
 import { weatherTool } from "./tools/weather/weather.js";
@@ -14,5 +17,31 @@ addTool(server);
 divideTool(server);
 weatherTool(server);
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
+if(process.env.TRANSPORT === 'stdio') {
+	const transport = new StdioServerTransport();
+	await server.connect(transport);
+}
+
+if(process.env.TRANSPORT === 'http') {
+	const transport = new StreamableHTTPServerTransport({
+		sessionIdGenerator: randomUUID
+	});
+	const app = express();
+	
+	app.use(express.json())
+
+	app.all('/mcp', (req, res) => {
+		transport.handleRequest(req, res, req.body)
+	});
+
+	const PORT = process.env.MCP_PORT ? Number.parseInt(process.env.MCP_PORT, 10) : 3000;
+
+	console.log(`Starting Express MCP server on port ${PORT}`);
+	console.log(`MCP endpoint: http://localhost:${PORT}/mcp`);
+
+	app.listen(PORT, () => {
+		console.log(`Server is running on http://localhost:${PORT}`)
+	})
+
+	await server.connect(transport);
+}
